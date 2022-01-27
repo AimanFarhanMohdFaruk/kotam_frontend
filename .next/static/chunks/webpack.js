16dfd03b1daf5c8b03000542644e177795153262
Aiman Farhan <aimanfaruk98@gmail.com>
2022-01-27 21:11:35 +0800

    building auth function

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "16bb6ab03d338039"; }
+/******/ 		__webpack_require__.h = function() { return "3a6b53715a205dd1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Auth/Auth.js
+++ components/Auth/Auth.js
@@ -1,6 +1,8 @@
 import styles from "./auth.module.css"
+import {useMutation, gql } from "@apollo/client"
 import Link from "next/link";
 import { useState } from "react";
+import { useRouter } from "next/router";
 import Image from "next/image";
 import {UserCircleIcon} from "@heroicons/react/solid"
 
@@ -9,8 +11,10 @@ const initialState = {username: "", firstName:"", lastName:"" ,email:"", passwor
 
 const Auth = () => {
 
+    const router = useRouter()
     const [ isSignUp, setIsSignUp ] = useState(true)
     const [ formData, setFormData ] = useState(initialState)
+
 
     const handleChange = (e) => {
         setFormData({...formData, [e.target.name]:e.target.value })
@@ -21,18 +25,26 @@ const Auth = () => {
         setIsSignUp((prevIsSignUp) => !prevIsSignUp)
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+
+
+        // router.push("/")
+    }
+
     return (
         <>
         <div className={styles.contentContainer}>
                 <div className={styles.formContainer}>
                     <UserCircleIcon width="5rem" height="5rem"/>
-                    <form className={styles.formContent}>
+                    <form  onSubmit={handleSubmit} className={styles.formContent}>
                         <h2>{isSignUp ? "Let's create an account!" : "Login"}</h2>
                         
                         {
                             isSignUp &&(
                                 <>
-                        <div className={styles.inputContainer1}>
+                        <div className={styles.inputContainer}>
                             <label for="username" > Username </label>
                             <input type="text" placeholder="" id="username" onChange={handleChange}/>
                         </div>
@@ -79,8 +91,10 @@ const Auth = () => {
                         )}
                         
                         <button  type="submit" className={styles.button}>{isSignUp ? "Sign Up" : "Log In"}</button>
-
+                        
+                        {isSignUp ? (
                         <p>Already have an account? <a className={styles.loginLink} onClick={switchMode}>Login here</a></p>
+                        ): <p>Don't have an account? <a className={styles.loginLink} onClick={switchMode}>Sign up here</a></p>}
 
                     </form>
                 </div>
